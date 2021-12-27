738465cf0efef9707f1995a09481cf67dc50ec49
RodroVMS <democlesvanshepars@gmail.com>
2021-12-27 00:03:41 -0500

    Add test

--- src/agent.spec.ts
+++ src/agent.spec.ts
@@ -1,10 +1,31 @@
-import {
-  FindingType,
-  FindingSeverity,
-  Finding,
-  HandleTransaction,
-  createTransactionEvent,
-} from "forta-agent";
-import agent from "./agent";
+import { createTransactionEvent, ethers } from "forta-agent";
+import { GOVERNOR_BRAVO_ADDRESS } from "./const";
 
-describe("high gas agent", () => {});
+var spawn = require("child_process").spawn;
+
+describe("high gas agent", () => {
+  it("aaa", async () => {
+    const provider = new ethers.providers.EtherscanProvider(
+      "homestead",
+      "7TY68RZP4EI89Z3YYYVVP9WB6TES3G8HAY"
+    );
+    //const gov = new ethers.Contract(GOVERNOR_BRAVO_ADDRESS, abi, provider);
+
+    const f = async () => {
+      let txs = provider.getHistory(GOVERNOR_BRAVO_ADDRESS);
+      return txs;
+    };
+    let txs = await f();
+    console.log("Got logs");
+    let count = 0;
+    txs.forEach((tx) => {
+      console.log("start foreach");
+      if (count > 1) return;
+      count = count + 1;
+      const txAddress = tx.hash;
+      console.log("spawning");
+      var prc = spawn("npm", ["run", "tx", txAddress]);
+      console.log("endspawn");
+    });
+  });
+});
