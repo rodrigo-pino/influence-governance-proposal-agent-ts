5a082f14df5beeced4927d829f39f1cba378cbd1
RodroVMS <democlesvanshepars@gmail.com>
2021-12-26 19:29:19 -0500

    Add agent transaction and block detection

--- src/agent.ts
+++ src/agent.ts
@@ -9,28 +9,133 @@ import {
   FindingType,
   ethers,
   getJsonRpcUrl,
-  getEthersProvider,
 } from "forta-agent";
-import { GOVERNANCE_ADDRESS } from "./const";
-
-import governance_abi from "./governance_abi.json";
+import {
+  GOVERNOR_BRAVO_ADDRESS,
+  UNI_ABI,
+  UNI_ADDRESS,
+  VOTE_CAST_SIG,
+} from "./const";
 
 const provider = new ethers.providers.JsonRpcProvider(getJsonRpcUrl());
-const governance = new ethers.Contract(
-  GOVERNANCE_ADDRESS,
-  governance_abi,
-  provider
-);
+const uni = new ethers.Contract(UNI_ADDRESS, UNI_ABI, provider);
 
-// Map voters from previous blocks
-const previousVoters: Map<string, number> = new Map();
+const previousVoters: Array<VoterTrack> = new Array();
 
+class VoterTrack {
+  address!: string;
+  votes!: BigNumber;
+  blockNum!: number;
+  suspicius: boolean = false;
+}
+
+const handleTransaction: HandleTransaction = async (
+  transactionEvent: TransactionEvent
+) => {
+  const findings: Finding[] = [];
+
+  console.log("handling transaction");
+
+  const voteCasts = transactionEvent.filterLog(
+    VOTE_CAST_SIG,
+    GOVERNOR_BRAVO_ADDRESS
+  );
+
+  const blockNum = transactionEvent.blockNumber;
+  console.log(`Transaction ocurred in blocknum: ${blockNum}`);
+  for (const vote of voteCasts) {
+    const voterAddress = vote.args.voter;
+    const priorVotes = await uni.getPriorVotes(voterAddress, blockNum - 100);
+
+    const voterVotes = vote.args.votes;
+
+    console.log(`votes: ${voterVotes}`);
+
+    //
+    let suspicius = false;
+    if (voterVotes - (await priorVotes) >= 0) {
+      suspicius = true;
+      findings.push(
+        Finding.fromObject({
+          name: "Significant balance Increase",
+          description: "It increased like, a lot",
+          alertId: "UNI-INC-1",
+          severity: FindingSeverity.Medium,
+          type: FindingType.Suspicious,
+        })
+      );
+    }
+
+    // Keep track of all voters to check transaction changes
+    previousVoters.push({
+      address: voterAddress,
+      votes: voterVotes,
+      blockNum: blockNum,
+      suspicius: suspicius,
+    });
+  }
+
+  return findings;
+};
+
+// For each block check if any voter had it's uni balance decreased
 const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
   const findings: Finding[] = [];
 
+  // Delete all voters which voted 100 or more blocks ago
+  while (
+    previousVoters.length > 0 &&
+    blockEvent.blockNumber - previousVoters[0].blockNum < 100
+  ) {
+    previousVoters.shift();
+  }
+
+  // Get the current account balance of every voter
+  const currentBalance = previousVoters.map(async (voter) =>
+    uni.getPriorVotes(voter.address)
+  );
+
+  const deleteIndices: Array<number> = [];
+  for (let i = 0; i < previousVoters.length; i++) {
+    const voter = previousVoters[i];
+    const currentVote = await currentBalance[i];
+
+    // If balance decreased
+    if (currentVote < voter.votes) {
+      let finding: Finding;
+      // Throw a different alert if balance decreased
+      // from an already suspicius account
+      if (voter.suspicius) {
+        finding = Finding.fromObject({
+          name: "Account balance decrease",
+          description: "Account balance decreased",
+          alertId: "UNI-DEC-2",
+          severity: FindingSeverity.High,
+          type: FindingType.Suspicious,
+        });
+      } else {
+        finding = Finding.fromObject({
+          name: "Account balance decrease",
+          description: "Account balance decreased",
+          alertId: "UNI-DEC-1",
+          severity: FindingSeverity.Medium,
+          type: FindingType.Suspicious,
+        });
+      }
+      deleteIndices.push(i);
+      // Add finding
+      findings.push(finding);
+    }
+  }
+
+  // Stop tracking suspicius voters
+  for (const index of deleteIndices.reverse()) {
+    previousVoters.splice(index, 1);
+  }
   return findings;
 };
 
 export default {
   handleBlock,
+  handleTransaction,
 };
